ad40cdd9fb221d019eb5e9e6a26e17f63dda75aa
Vinay <Zzoey@users.noreply.github.com>
2021-11-21 02:03:36 +0000

    Update inference.py

--- code/inference.py
+++ code/inference.py
@@ -126,6 +126,4 @@ def main():
 if __name__ == "__main__":
     operate_on_dirs_inference()
     main()  # runs inference on the test set of the FashionMNIST
-    inference_on_set_of_images(
-        input_path, output_path
-    )  # run inference on set of images from a folder
+    #inference_on_set_of_images(input_path, output_path)  # run inference on set of images from a folder
